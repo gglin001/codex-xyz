9c0b3bfaf4c515e5c3d45aede9541d0aba2a0fed
Allen Guo <guosonglin001@gmail.com>
2026-06-23 00:38:37 +0800

    misc

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# http://127.0.0.1:1123
-export COZ_UI_URL="http://127.0.0.1:1123"
+# default http://127.0.0.1:1123
 pnpm run dev
 
 COZ_DEBUG_LEVEL=1 pnpm run dev
 COZ_DEBUG_LEVEL=2 pnpm run dev
 COZ_DEBUG_LEVEL=3 pnpm run dev
 
-# custom env
-# http://100.64.0.4:1123
-export COZ_UI_URL="http://100.64.0.1:1123"
-# export COZ_UI_URL="http://100.64.0.4:1123"
+# prefer using ip 100.64.x.y with tailscale, eg: http://100.64.0.1:1123
+IP=$(ifconfig | sed -E -n 's/^[[:space:]]*inet (100\.64\.[0-9]{1,3}\.[0-9]{1,3}).*/\1/p')
+export COZ_UI_URL="http://${IP}:1123"
 export CODEX_HOME="${PWD}/dot.home/.codex"
 COZ_DEBUG_LEVEL=2 pnpm run dev
