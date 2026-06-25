73965cc81a36a5cd2bcc18d258dee18f2cb92d95
Allen Guo <guosonglin001@gmail.com>
2026-06-25 13:54:21 +0000

    IPV4

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -2,11 +2,11 @@
 set -euo pipefail
 
 # using COZ_UI_IP 100.64.x.y with tailscale, eg: http://100.64.0.1:1123
-# TAIL_IP=$(ifconfig | sed -E -n 's/^[[:space:]]*inet (100\.64\.[0-9]{1,3}\.[0-9]{1,3}).*/\1/p')
-# COZ_UI_IP="${COZ_UI_IP:-$TAIL_IP}"
-LOCAL_IP="127.0.0.1"
+# IPV4=$(ifconfig | sed -E -n 's/^[[:space:]]*inet (100\.64\.[0-9]{1,3}\.[0-9]{1,3}).*/\1/p')
+# IPV4=$(ip -4 a | sed -E -n 's/^[[:space:]]*inet (192\.168\.[0-9]{1,3}\.[0-9]{1,3}).*/\1/p')
+IPV4="127.0.0.1"
 
-export COZ_UI_IP="${COZ_UI_IP:-$LOCAL_IP}"
+export COZ_UI_IP="${COZ_UI_IP:-$IPV4}"
 export COZ_UI_PORT="${COZ_UI_PORT:-1123}"
 
 export CODEX_HOME="${PWD}/dot.home/.codex"
