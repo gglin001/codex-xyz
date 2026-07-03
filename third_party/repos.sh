ed70d259244a15c2e30d7205b6a775ac771d66a5
Allen Guo <guosonglin001@gmail.com>
2026-07-03 14:15:17 +0800

    3rd

--- third_party/repos.sh
+++ third_party/repos.sh
@@ -4,5 +4,7 @@ set -euo pipefail
 pushd third_party
 
 git clone org-14957082@github.com:openai/codex.git --single-branch
+git clone git@github.com:earendil-works/pi.git --single-branch
+git clone git@github.com:n0-computer/iroh.git --single-branch
 
 popd
